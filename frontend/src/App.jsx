ef913f4cc12ddc9e369c0f8c0593f0afa16aa876
rohit-kotiyal <rohitkotiyal96@gmail.com>
2026-02-13 11:22:06 +0530

    Configured Vite+React+TailwindCSS

---
+++ frontend/src/App.jsx
@@ -0,0 +1,7 @@
+export default function App(){
+  return (
+    <div className="w-full h-96 flex justify-center items-center">
+      <h1 className="text-4xl text-blue-600 font-bold">Tailwind Is Working</h1>
+    </div>
+  )
+}
